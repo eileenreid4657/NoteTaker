fb498bb2f7c1f7c6d71b6fc37d22134926fc6b5b
Eileen Reid <eileenreid4657@gmail.com>
2020-03-09 10:54:35 -0400

    Updating Notetaker

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+app.use(express.static("public"))
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
